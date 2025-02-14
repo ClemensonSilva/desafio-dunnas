2fa773b548ace9dd23e89c111f5e3d8116da598e
ClemensonSilva <clemesonsilva736@gmail.com>
2025-02-14 14:00:52 -0300

    inclusao de relacionamento entre as tabelas funcionarios e setors, 1 setor has_many e funcionario belongs_to

--- app/models/funcionario.rb
+++ app/models/funcionario.rb
@@ -1,2 +1,3 @@
 class Funcionario < ApplicationRecord
+  belongs_to :setor, foreign_key: 'setor_id'
 end
--- app/models/setor.rb
+++ app/models/setor.rb
@@ -1,3 +1,4 @@
 class Setor < ApplicationRecord
   belongs_to :unidade, foreign_key: 'unidade_id'
+  has_many :funcionarios
 end
